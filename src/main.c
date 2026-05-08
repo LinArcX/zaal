6fc5de1ef4f22d3f0ce107a0f5158db104bd468c
linarcx <linarcx@gmail.com>
2026-05-08 16:04:46 +0200

    updated

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdbool.h>
 
 #include "scanner.h"
+#include "zutil.h"
+#include "util/zaalStrings.h"
+
 //#include "parser.h"
 //#include "generator.h"
 
@@ -10,54 +13,66 @@ int main(int argc, const char* argv[])
   int result = -1;
 
   if (2 != argc) {
-    fprintf(stderr, "Usage: %s <sourceFile>\n", argv[0]);
+    fprintf(stderr, "Usage: %s <sourceFile>.zl\n", argv[0]);
   }
   else {
-    const FILE* pSourceFile = fopen(argv[1], "r");
-    if (NULL == pSourceFile) {
-      fprintf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
-      pSourceFile = NULL;
-    }
-    else {
-      Token token = {0};
-      uint32_t line = 1;
+    char ext[MAX_STRING_SIZE] = {0};
 
-      char Text[ETextLength] = {0};
-
-      // sometimes we need to "put back" a character if we have already read too far ahead in the input stream. 0 in ASCII mean NULL
-      uint32_t putBackChar = 0;  
-
-      if(true == scan(pSourceFile, &token, &line, &putBackChar, &Text)) {
-        FILE* pOutFile = fopen("out.s", "w");
-        if (NULL == pOutFile) {
-          fprintf(stderr, "Unable to create out.s: %s\n", strerror(errno));
-          pOutFile = NULL;
+    if (getExtensionOfFile(argv[1], ext, MAX_FILE_EXTENSION_SIZE)) {
+      if (0 == strcmp(ext, "zl")) {
+        const FILE* pSourceFile = fopen(argv[1], "r");
+        if (NULL == pSourceFile) {
+          fprintf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
+          pSourceFile = NULL;
         }
         else {
-          //genpreamble();                
-          //statements();
-          //genpostamble();               
-          //struct ASTnode *node = {0};
-          //node = parseExpressions(0);		
-          //if(NULL != node)
-          //{
-          //  printf("%d\n", interpretAST(node));
-          //  generateCode(node);
-          //  fclose(pOutFile);
-          //  return 0;
-          //}
+          Token token = {0};
+          uint32_t line = 1;
 
-          if (fclose(pOutFile) != 0) {
-            perror("fclose");
+          char Text[ETextLength] = {0};
+
+          // sometimes we need to "put back" a character if we have already read too far ahead in the input stream. 0 in ASCII mean NULL
+          uint32_t putBackChar = 0;  
+
+          if(true == scan(pSourceFile, &token, &line, &putBackChar, &Text)) {
+            FILE* pOutFile = fopen("out.s", "w");
+            if (NULL == pOutFile) {
+              fprintf(stderr, "Unable to create out.s: %s\n", strerror(errno));
+              pOutFile = NULL;
+            }
+            else {
+              //genpreamble();                
+              //statements();
+              //genpostamble();               
+              //struct ASTnode *node = {0};
+              //node = parseExpressions(0);		
+              //if(NULL != node)
+              //{
+              //  printf("%d\n", interpretAST(node));
+              //  generateCode(node);
+              //  fclose(pOutFile);
+              //  return 0;
+              //}
+
+              if (fclose(pOutFile) != 0) {
+                perror("fclose");
+              }
+              pOutFile = NULL;
+              result = 0;
+            }
           }
-          pOutFile = NULL;
-          result = 0;
+          if (fclose(pSourceFile) != 0) {
+            perror("ERROR! fclose: pSourceFile");
+          }
+          pSourceFile = NULL;
         }
       }
-      if (fclose(pSourceFile) != 0) {
-        perror("ERROR! fclose: pSourceFile");
+      else {
+        fprintf(stderr, "Wrong extension. please provide a file with .zl extension.\n");
       }
-      pSourceFile = NULL;
+    }
+    else {
+      fprintf(stderr, "%s has no extension. please provide a file with .zl extension.\n", argv[1]);
     }
   }
   return result;
